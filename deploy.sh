a91b1d6aa52f48f576c71afc7e1f6c378fafb4a7
Zander Hill <Zander@civet.ws>
2014-02-12 18:03:17 -0500

    Mod deploy

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ bundle exec rake gen_deploy && \
   git stash && \
   git push && \
   git checkout master && \
-  rm -rf && \
+  rm -rf "${pwd}/*" && \
   git checkout source -- _deploy && \
   cp -r _deploy/* . && \
   rm -rf _deploy/ && \
